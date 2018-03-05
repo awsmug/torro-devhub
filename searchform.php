cd31a8cc41eca5ec60920a5335c7441df4201af5
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-03-05 13:32:36 -0500

    Enhance reference overview page.

--- searchform.php
+++ searchform.php
@@ -7,7 +7,17 @@
  * @link    http://developer.torro-forms.com
  */
 
+global $is_reference_page;
+
 $unique_id = esc_attr( uniqid( 'search-form-' ) );
+
+$post_types = torro_devhub_devhub_get_post_types();
+$taxonomies = torro_devhub_devhub_get_taxonomies();
+
+$is_reference = false;
+if ( ! empty( $is_reference_page ) || ! empty( $_GET['reference'] ) || is_singular( $post_types ) || is_post_type_archive( $post_types ) || is_tax( $taxonomies ) ) {
+	$is_reference = true;
+}
 
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
@@ -15,8 +25,19 @@ $unique_id = esc_attr( uniqid( 'search-form-' ) );
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'torro-devhub' ); ?></span>
 	</label>
 	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'torro-devhub' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<?php if ( 0 === strpos( $_SERVER['REQUEST_URI'], '/reference' ) || ! empty( $_GET['reference'] ) ) : ?>
+	<button type="submit" class="search-submit"><?php echo torro_devhub_get_svg( 'search' ); ?><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'torro-devhub' ); ?></span></button>
+	<?php if ( ! empty( $is_reference_page ) ) : ?>
+		<div class="search-post-type">
+			<?php $active_post_types = ( ! empty( $_GET['post_type'] ) && is_array( $_GET['post_type'] ) ) ? $_GET['post_type'] : array(); ?>
+			<span><?php esc_html_e( 'Filter by type:', 'torro-devhub' ); ?></span>
+			<?php foreach ( torro_devhub_devhub_get_post_types( true, true ) as $post_type => $label ) : ?>
+				<label>
+					<input type="checkbox" name="post_type[]" value="<?php echo esc_attr( $post_type ); ?>"<?php echo in_array( $post_type, $active_post_types, true ) ? ' checked' : ''; ?>>
+					<?php echo esc_html( $label ); ?>
+				</label>
+			<?php endforeach; ?>
+		</div>
+	<?php elseif ( ! empty( $is_reference ) ) : ?>
 		<input type="hidden" name="reference" value="1">
 	<?php endif; ?>
-	<button type="submit" class="search-submit"><?php echo torro_devhub_get_svg( 'search' ); ?><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'torro-devhub' ); ?></span></button>
 </form>
--- templates/reference.php
+++ templates/reference.php
@@ -9,6 +9,10 @@
  * @link    http://developer.torro-forms.com
  */
 
+global $is_reference_page;
+
+$is_reference_page = true;
+
 get_header();
 
 ?>
@@ -18,18 +22,29 @@ get_header();
 		<?php
 		while ( have_posts() ) : the_post();
 
+			?>
+			<div class="page-search-form">
+				<h2><?php esc_html_e( 'Search through the codebase', 'torro-devhub' ); ?></h2>
+
+				<p><?php esc_html_e( 'Enter the name of a function, method, hook, class, trait or interface to learn more.', 'torro-devhub' ); ?></p>
+				<?php get_search_form(); ?>
+
+				<p><?php esc_html_e( 'Or browse through the respective lists:', 'torro-devhub' ); ?></p>
+				<ul class="inline-list"
+					<?php foreach ( torro_devhub_devhub_get_post_types( true, true ) as $post_type => $label ) : ?>
+						<li>
+							<a href="<?php echo esc_url( get_post_type_archive_link( $post_type ) ); ?>"><?php echo esc_html( $label ); ?></a>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			</div>
+			<?php
+
 			if ( torro_devhub_use_post_format_templates() ) :
 				get_template_part( 'template-parts/content/content-' . get_post_type(), get_post_format() );
 			else :
 				get_template_part( 'template-parts/content/content', get_post_type() );
 			endif;
-
-			?>
-			<div class="page-search-form">
-				<p><?php esc_html_e( 'Enter the name of a function, method, hook, class, trait or interface to learn more.', 'torro-devhub' ); ?></p>
-				<?php get_search_form(); ?>
-			</div>
-			<?php
 
 			if ( torro_devhub_display_post_navigation() ) :
 				the_post_navigation();
@@ -48,3 +63,5 @@ get_header();
 
 get_sidebar();
 get_footer();
+
+$is_reference_page = false;
